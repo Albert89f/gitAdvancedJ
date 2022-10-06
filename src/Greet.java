85bb7de7fc99ee00f5cf5feda1d65c13e8b543d2
SomaSlept <99564394+SomaSlept@users.noreply.github.com>
2022-10-06 19:41:36 +0200

    c5

--- src/Greet.java
+++ src/Greet.java
@@ -1,12 +1,7 @@
 public class Greet {
-<<<<<<< HEAD
-    public void greet(){
 
-        System.out.println("Greetings!");
-
-=======
     public void greet(String name1, String name2){
         System.out.println("Hello " + name1 + " and " + name2);
->>>>>>> b7b236229aecc7c9ca5ec82ba5560e455fd8cf42
+
     }
 }
